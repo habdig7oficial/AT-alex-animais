9d810f68ff4f848f83b103966fdc25d32ce4cc2b
Mateus Felipe Vieira <habdig7@gmail.com>
2023-04-04 13:37:49 +0000

    aula4abr

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,34 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+import { animal_obj, animais, respostas, perguntas } from './animais';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonicModule,NgFor, NgIf],
+})
+
+
+
+export class HomePage {
+
+  debug = true
+
+  animals = animais
+  start: boolean = false
+  perguntas: Array<string> = perguntas
+
+  j:number = 0 
+  constructor() {}
+
+  StoreANS(ans : boolean){
+    respostas.push(ans)
+    console.log(respostas)
+    
+  }
+
+}
